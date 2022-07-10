07e95c008c7e66966361a8953999b8d522dc8f1d
Dennis Gabriel <dennisgabr745@gmail.com>
2022-07-10 18:18:04 -0300

    Algumas Melhorias

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function perfilInstagram(){
+    window.open('https://instagram.com/dennis_gabs')
+}
+
+function perfilLinkedin(){
+    window.open('https://www.linkedin.com/in/dennis-gabriel-b482821ab/')
+}
+
+function perfilGitHub(){
+    window.open('https://github.com/dennisgabriel-dev');    
+}
